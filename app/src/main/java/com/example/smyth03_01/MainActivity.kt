86fcd0c177e8f7dd0ddf913a693ac65d49cd9f52
Weiqifan1 <cmlykke@hotmail.com>
2019-04-15 09:22:59 +0200

    1597/22642 appen virker, dollar til euro converteres. eksemplet slut

--- app/src/main/java/com/example/smyth03_01/MainActivity.kt
+++ app/src/main/java/com/example/smyth03_01/MainActivity.kt
@@ -5,10 +5,22 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    fun convertCurrency(view: View){
+        if(dollarText.text.isNotEmpty()){
+            val dollarValue = dollarText.text.toString().toFloat()
+            val euroValue = dollarValue * 0.85f
+            textView.text = euroValue.toString()
+        }else{
+            textView.text = "No Value"
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,4 +48,6 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+
 }
